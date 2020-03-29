74e5509f721a93919eab57470abf00d53130773b
Lucas Andrade <llucasandradd@gmail.com>
2020-03-29 13:13:53 -0300

    Start to create GroupForm #13

--- src/components/Groups/GroupForm.js
+++ src/components/Groups/GroupForm.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import { Form } from '../Common';
+import React, { useState } from 'react';
+import browser from 'webextension-polyfill';
+import { Form, Input, Button } from '../Common';
 
 const GroupForm = () => {
-  return <Form>Lucas</Form>;
+  const [inputValue, setInputValue] = useState('');
+
+  const saveGroup = () => {
+    browser.storage.local.get('groups').then(({ groups = {} }) => {
+      if (groups[inputValue]) return null;
+
+      groups[inputValue] = {
+        name: inputValue,
+        startTime: '',
+        endTime: '',
+        sitesList: [],
+      };
+      browser.storage.local.set({ groups });
+      return true;
+    });
+  };
+
+  const handleInputChange = event => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  return (
+    <Form>
+      <Input value={inputValue} onChange={e => handleInputChange(e)} />
+      <Button onClick={() => saveGroup()} mt={16}>
+        Add Group
+      </Button>
+    </Form>
+  );
 };
 
 export default GroupForm;
