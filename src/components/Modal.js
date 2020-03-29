ca2b7cc8ad817e358723469b9a4ad6e3fcf39e75
Lucas Andrade <llucasandradd@gmail.com>
2020-03-28 23:06:41 -0300

    Move all content logic to inside of Modal component #12

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import browser from 'webextension-polyfill';
-import styled from 'styled-components';
+import React from 'react';
+import theme from 'themes/default';
+import styled, { ThemeProvider } from 'styled-components';
+import { H1, H3, Quote } from './Common';
+import { quotes } from '../assets/quotes.json';
+import { useWebsitesList } from '../hooks';
 
 const Modal = () => {
-  // useEffect(() => {
-  //   browser.tabs.getCurrent(function (tabId, changeInfo, tab) {
-  //     console.log(tabId);
-  //     console.log(changeInfo);
-  //     console.log(tab);
-  //   });
-  // }, []);
+  const sitesList = useWebsitesList();
 
-  return <h3>lucas andrade</h3>;
+  const quote = quotes[(Math.random() * quotes.length) | 0];
+
+  if (!sitesList.includes(window.location.origin)) return null;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <ModalContent>
+          <H1>{`Hey, you should't be here\nTry to keep focus\nYou can do it`}</H1>
+          <H3 size="2em">Maybe these words can help</H3>
+          <Quote>{`"${quote.text}"`}</Quote>
+          <Quote>{quote.author}</Quote>
+        </ModalContent>
+      </Container>
+    </ThemeProvider>
+  );
 };
+
+const Container = styled.div`
+  position: fixed;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+  z-index: 10000;
+  background-color: rgba(0, 0, 0, 0.95);
+`;
+
+const ModalContent = styled.div`
+  position: absolute;
+  z-index: 10500;
+  background: #ffffff;
+  width: 550px;
+  height: 350px;
+  padding: 20px 50px;
+  border-radius: 1px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 export default Modal;
--- src/content.js
+++ src/content.js
@@ -1,68 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import theme from 'themes/default';
-import styled, { ThemeProvider } from 'styled-components';
-import { H1, H3, Quote } from './components/Common';
-import { quotes } from './assets/quotes.json';
-import { useWebsitesList } from './hooks';
-
-const Modal = () => {
-  const sitesList = useWebsitesList();
-
-  const quote = quotes[(Math.random() * quotes.length) | 0];
-
-  if (!sitesList.includes(window.location.origin)) return null;
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <ModalContent>
-          <H1>{`Hey, you should't be here\nTry to keep focus\nYou can do it`}</H1>
-          <QuoteContainer>
-            <H3 size="2em">Maybe these words can help</H3>
-            <QuoteText>
-              <Quote>{`"${quote.text}"`}</Quote>
-              <Quote>{quote.author}</Quote>
-            </QuoteText>
-          </QuoteContainer>
-        </ModalContent>
-      </Container>
-    </ThemeProvider>
-  );
-};
-
-const Container = styled.div`
-  position: fixed;
-  margin: 0 auto;
-  width: 100%;
-  height: 100%;
-  z-index: 10000;
-  background-color: rgba(0, 0, 0, 0.95);
-`;
-
-const ModalContent = styled.div`
-  position: absolute;
-  z-index: 10500;
-  background: #ffffff;
-  width: 550px;
-  height: 350px;
-  padding: 20px 50px;
-  border-radius: 1px;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const QuoteContainer = styled.div`
-  position: relative;
-  height: 50%;
-  padding-top: 50px;
-`;
-
-const QuoteText = styled.div`
-  position: absolute;
-  bottom: 10px;
-`;
+import Modal from './components/Modal';
 
 const app = document.createElement('div');
 app.id = 'modal-root';
