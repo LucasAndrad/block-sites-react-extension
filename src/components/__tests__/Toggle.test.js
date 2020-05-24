ce427c1e43783da39e14aea7cb62bb77c2877d82
Lucas Andrade <llucasandradd@gmail.com>
2020-05-24 15:06:55 -0300

    Add React and ReactDOM packages

--- src/components/__tests__/Toggle.test.js
+++ src/components/__tests__/Toggle.test.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import Toggle from 'src/Toggle';
+import Toggle from '../Toggle';
 
 describe('Toggle component ON and OFF', () => {
   it('should render Toggle and change between ON and OFF', () => {
     let active = false;
-    const changeActive = () => active = !active;
+    const changeActive = () => {
+      active = !active;
+    };
     render(<Toggle active={active} toggleActive={changeActive} />);
-    fireEvent.click(screen.findByTestId('toggle-btn'));
+    fireEvent.click(screen.getByTestId('toggle-btn'));
     expect(active).toBe(true);
-    fireEvent.click(screen.findByTestId('toggle-btn'));
+    fireEvent.click(screen.getByTestId('toggle-btn'));
     expect(active).toBe(false);
   });
 });
