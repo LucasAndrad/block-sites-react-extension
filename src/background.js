208b9fd73301de0f2526e3e363d45d632b1a9acd
Lucas Andrade <llucasandradd@gmail.com>
2020-06-07 16:03:03 -0300

    Remove unused component Example

--- src/background.js
+++ src/background.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
-import Example from 'components/Example';
 import defaultTheme from 'themes/default';
 
 const backgroundPage = () => {
